7473683c0095e03cfeabad091286b02724915f22
Declan Whitford Jones <wjdeclan@gmail.com>
2016-12-10 15:30:28 -0500

    Game controls

--- core/src/com/quickjam3/QuickJam.java
+++ core/src/com/quickjam3/QuickJam.java
@@ -1,33 +1,135 @@
 package com.quickjam3;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
-public class QuickJam extends ApplicationAdapter {
+public class QuickJam extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture enemyTexture, playerTexture;
+
+	PlayerController player;
+
+	ArrayList<WallBounceSprite> enemies;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		enemyTexture = new Texture("enemy.png");
+		playerTexture = new Texture("player.png");
+
+		player = new PlayerController(playerTexture);
+
+		player.setPosition((768 - player.getWidth()) / 2, (576 - player.getHeight()) / 2);
+
+		populate();
+		
+		Gdx.input.setInputProcessor(this);
+	}
+
+	public void populate() {
+		enemies = new ArrayList<WallBounceSprite>();
+
+		for (int i = 0; i < 50; i++) {
+			WallBounceSprite wbs = new WallBounceSprite(enemyTexture);
+			wbs.setPosition((float) (Math.random() * 768 - wbs.getWidth()),
+					(float) (Math.random() * 576 - wbs.getHeight()));
+		}
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		float dt = Gdx.graphics.getDeltaTime();
+
+		for (WallBounceSprite enemy : enemies) {
+			enemy.update(dt);
+		}
+
+		player.update(dt);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		for (WallBounceSprite enemy : enemies) {
+			enemy.draw(batch);
+		}
+
+		player.draw(batch);
+
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+
+		enemyTexture.dispose();
+		playerTexture.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if (keycode == Input.Keys.LEFT) {
+			player.getMovement().set(new Vector2(-100, player.movement.y));
+		} else if (keycode == Input.Keys.RIGHT) {
+			player.getMovement().set(new Vector2(100, player.movement.y));
+		} else if (keycode == Input.Keys.UP) {
+			player.getMovement().set(new Vector2(player.movement.x, 100));
+		} else if (keycode == Input.Keys.DOWN) {
+			player.getMovement().set(new Vector2(player.movement.x, -100));
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
